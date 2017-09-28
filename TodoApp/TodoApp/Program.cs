ddb806bdc7bf7e3d4f922a9228c90e4fefd5d61f
Lorand <szilagyi.lori@gmail.com>
2017-09-28 11:41:04 +0200

    welcome text added

---
+++ TodoApp/TodoApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome();
+            Console.ReadLine();
+        }
+        public static void Welcome()
+        {
+            Console.WriteLine("Command Line Todo application\n=============================\n\nCommand line arguments:\n -l   Lists all the tasks\n -a   Adds a new task\n -r   Removes an task\n -c   Completes an task\n");
+        }
+        
+    }
+}
