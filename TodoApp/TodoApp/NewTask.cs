52ab43cfc5babf6de3753d280d0e9bc6743ccfdc
Lorand <szilagyi.lori@gmail.com>
2017-09-28 17:19:49 +0200

    monkey tricks me

---
+++ TodoApp/TodoApp/NewTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TodoApp
+{
+    class NewTask
+    {
+        private string[] text;
+        private string path;
+
+        public NewTask()
+        {
+            path = @"./datastore.txt";
+        }
+
+        public NewTask(string[] text, string path)
+        {
+            this.text = text;
+            this.path = path;
+        }
+
+        public void AddNewTask()
+        {
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                writer.WriteLine("Feed the monkey");          
+            }
+        }
+    }
+}
--- TodoApp/TodoApp/Program.cs
+++ TodoApp/TodoApp/Program.cs
@@ -9,25 +9,24 @@ namespace TodoApp
             Welcome();
 
             TaskList taskList = new TaskList();
+            NewTask newTask = new NewTask();
 
-            //if (args == null)
-            //{
-            //    return;
-            //}
             if (args[0] == "-l")
             {
                 taskList.ReadFile();
             }
-
+            if (args[0] == "-a")
+            {
+                newTask.AddNewTask();
+            }
+            
             string[] commands = { "-l", "-a", "-r", "-c" };
-
 
             Console.ReadLine();
         }
         public static void Welcome()
         {
             Console.WriteLine("Command Line Todo application\n=============================\n\nCommand line arguments:\n -l   Lists all the tasks\n -a   Adds a new task\n -r   Removes an task\n -c   Completes an task\n");
-        }
-        
+        } 
     }
 }
